7eb92bdee1da72e4c3772d7f51e417ddaad05ec3
alyssoncastro <alysson.cordeiro@sou.inteli.edu.br>
2024-09-26 11:08:44 -0300

    incremento do front

--- src/frontend/carbon-check/src/pages/dashboard.tsx
+++ src/frontend/carbon-check/src/pages/dashboard.tsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Box, Typography, Paper, Grid } from '@mui/material';
-=======
-import React, {useState, useEffect} from 'react';
-import {Button, Stack, Box} from '@mui/material';
-import localCloud from './cloudIcon.svg'
-import Grid from '@mui/material/Grid2';
-
->>>>>>> fe6009e79e6150fda17522f80cee7f831f0d618f
+import React, { useState, useEffect } from 'react';
+import { Box, Typography, Paper, Grid, Stack } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
 import CompareIcon from '@mui/icons-material/Compare';
@@ -15,6 +7,7 @@ import CompareImgComponent from '../components/CompareImgComponent';
 import TreeNumber from '../components/TreeNumber';
 import TrustComponent from '../components/TrustComponent';
 import Navbar from '../components/Navbar';
+import CustomButton from '../components/CustomButton';
 
 
 
@@ -49,10 +42,13 @@ const DashboardPage: React.FC = () => {
       console.error('Error fetching metrics:', error);
     }
   };
- // Fetch metrics whenever the numTrees value changes
+
+
  useEffect(() => {
   fetchMetrics();
 }, [numTrees]);
+
+
 
   // Rota para enviar a imagem pro backend
   const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -76,6 +72,22 @@ const DashboardPage: React.FC = () => {
         console.error('Error uploading file:', error);
       }
     }
+
+  };
+
+
+
+  //para os 3 botões do dashboard
+  const handleUploadClick = () => {
+    console.log("Upload clicked");
+  };
+
+  const handleAnalyzeClick = () => {
+    console.log("Analyze clicked");
+  };
+
+  const handleCompareClick = () => {
+    console.log("Compare clicked");
   };
 
 
@@ -83,56 +95,9 @@ const DashboardPage: React.FC = () => {
 
   return (
     <div>
-<<<<<<< HEAD
       
       <Navbar />
-=======
-      <h1>Dashboard</h1>
-    <Grid container spacing={1}>
-    <Grid size={5} offset={{ md: 2 }}>
-    <TrustComponent percentage={85} startYear={1903} endYear={2050} />
-    </Grid>
-    <Grid size={5}>
-    <TreeNumber totalTrees={1984} />
-    </Grid>
-    </Grid>
-    <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="20vh" 
-      >
-        <Stack direction="row" spacing={8}>
-          <Button  variant="outlined" color="success" startIcon={<CloudUploadIcon />}>
-          <label htmlFor="upload-input" style={{ cursor: 'pointer' }}>
-              Inserir imagem
-            </label>
-          </Button>
-          <input id="upload-input" type="file" style={{ display: 'none' }} onChange={handleFileUpload} />
-          <Button variant="outlined" startIcon={<AnalyticsIcon />}>
-            Analisar terreno
-          </Button>
-          <Button onClick={handleOpen} variant="outlined" startIcon={<CompareIcon />}>
-            Comparar imagens
-          </Button>
-          <CompareImgComponent open={open} handleClose={handleClose} />
-        </Stack>
-      </Box>
-      {metrics && (
-        <div>
-          <h2>Métricas Calculadas</h2>
 
-          <p>Além dos créditos de carbono as árvores também são responsáveis por um grande impacto no meio ambiente.</p>
-          <p>Os números abaixo indicam o impacto total das árvores em um período de 40 anos.</p>
-
-
-          <p>CO2 Sequestrado: {metrics.total_co2} kg</p>
-          <p>Oxigênio Produzido: {metrics.total_oxygen} kg</p>
-          <p>Água Retida: {metrics.total_water} litros</p>
-          <p>Solo Preservado: {metrics.total_soil} kg</p>
-        </div>
-      )}
->>>>>>> fe6009e79e6150fda17522f80cee7f831f0d618f
 
       {/* Adicionando o gradiente ao fundo */}
       <Box
@@ -146,15 +111,10 @@ const DashboardPage: React.FC = () => {
         }}
       >
         {/* Grid principal */}
-        <Grid
-          container
-          spacing={4}
-          justifyContent="center"
-          alignItems="center"
-          sx={{ padding: 10 }}
-        >
+        <Grid container spacing={4} justifyContent="center" alignItems="center" sx={{ padding: 10 }}>
 
-
+         
+           
 
           {/* Quadro 1: Intervalo de Árvores */}
           <Grid item xs={12} md={5}>
@@ -190,6 +150,36 @@ const DashboardPage: React.FC = () => {
               </Grid>
             </Paper>
           </Grid>
+
+
+          {/* adicionar os botões */}
+          <Grid item xs={15}>
+            <Stack direction="row" spacing={4} justifyContent="center">
+            
+              {/* Botão Inserir Imagem */}
+              <CustomButton 
+                label="Inserir imagem" 
+                iconSrc="/inserir_img.png" 
+                onClick={handleUploadClick} 
+              />
+
+              {/* Botão Analisar Terreno */}
+              <CustomButton 
+                label="Analisar terreno" 
+                iconSrc="/analisa_img.png" 
+                onClick={handleAnalyzeClick} 
+              />
+
+              {/* Botão Comparar Imagens */}
+              <CustomButton 
+                label="Comparar imagens" 
+                iconSrc="/compara_img.png" 
+                onClick={handleCompareClick} 
+              />
+            </Stack>
+          </Grid>
+
+
         </Grid>
       </Box>
     </div>
