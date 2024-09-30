99e9998ecd64bfc411e93f408285669dca08397b
021Antonio <antonio.cavalcante@sou.inteli.edu.br>
2024-09-30 01:59:32 -0300

    feat: template front

---
+++ src/frontend/carbon-check/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='bg-red-600'>hello world</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend/carbon-check/src/pages/Land-Page/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandPage = () => {
+  return (
+    <div>
+      <h1>LandPage
+    
+      </h1>
+    </div>
+  );
+};
+
+export default LandPage;
