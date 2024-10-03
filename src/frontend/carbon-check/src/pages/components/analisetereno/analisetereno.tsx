4143847734b9de09dd7e0cfc9220f59ec3fefeff
gabriel pascoli <gabriel.pascoli@sou.inteli.edu.br>
2024-10-03 11:29:27 -0300

    primeirobotão

---
+++ src/frontend/carbon-check/src/pages/components/analisetereno/analisetereno.tsx
@@ -0,0 +1,8 @@
+<button className="bg-black text-white py-2 px-4 rounded-[8%] flex items-center hover:bg-gray-800">
+  <img
+    src="/home/gabriel/2024-2A-T02-EC11-G05/src/frontend/carbon-check/src/assets/check.png"
+    alt="Check Icon"
+    className="h-6 w-6 mr-2"
+  />
+  Analisar Terreno
+</button>
